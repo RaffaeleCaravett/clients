7d0fe15465aa3709d5bb5965ed9b0d8d30306588
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-03-09 18:05:24 +0100

    logout done

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormsService } from 'src/app/services/forms.service';
 
 @Component({
@@ -9,9 +10,17 @@ import { FormsService } from 'src/app/services/forms.service';
 export class NavComponent {
 signedIn:boolean=false
 
-constructor(private formsService:FormsService){
+constructor(private formsService:FormsService,private router:Router){
   this.formsService.isAuthenticated.subscribe((bool:boolean)=>{
     this.signedIn=bool
   })
+}
+
+logout(){
+  localStorage.clear()
+  this.formsService.authenticateUser(false)
+  this.formsService.token=''
+  this.formsService.refreshToken=''
+  this.router.navigate(['/'])
 }
 }
